bc66204a1f9cc6abc9fbe7a2fb357cdb810cf086
Avery <jarreed0@gmail.com>
2017-02-14 09:39:17 -0700

    changed log to index

--- logger.sh
+++ logger.sh
@@ -12,19 +12,19 @@ echo -n "TODO: "
 read td;
 echo -n "How was your day? (from 1 - 100, 10 being the highest): "
 read md
-head -n -5 log.html > temp.txt
-cat temp.txt > log.html
-echo "<div class="entry" mood="$md">" >> log.html
-echo "   <span id="year">$yr</span>" >> log.html
-echo "   <span id="month">$mn</span>" >> log.html
-echo "   <span id="day">$dy</span>" >> log.html
-echo "   <span id="desc">$entry</span>" >> log.html
-echo "   <span id="todo">$td</span>" >> log.html
-echo "   <span id="time">`date`</span>" >> log.html
-echo "</div><p>" >> log.html
-echo " " >> log.html
-echo "</body>" >> log.html
-echo " " >> log.html
-echo "<footer>" >> log.html
-echo "</footer>" >> log.html
-echo "</html>" >> log.html
+head -n -5 index.html > temp.txt
+cat temp.txt > index.html
+echo "<div class="entry" mood="$md">" >> index.html
+echo "   <span id="year">$yr</span>" >> index.html
+echo "   <span id="month">$mn</span>" >> index.html
+echo "   <span id="day">$dy</span>" >> index.html
+echo "   <span id="desc">$entry</span>" >> index.html
+echo "   <span id="todo">$td</span>" >> index.html
+echo "   <span id="time">`date`</span>" >> index.html
+echo "</div><p>" >> index.html
+echo " " >> index.html
+echo "</body>" >> index.html
+echo " " >> index.html
+echo "<footer>" >> index.html
+echo "</footer>" >> index.html
+echo "</html>" >> index.html
